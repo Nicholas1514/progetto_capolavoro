577d0d250f556d462939954c479bfe37d32bf006
Zappa.20201 <Zappa.20201@E2-9-PC13>
2024-04-30 13:30:50 +0200

    Gestione della generazione degli indici delle squadre (senza ripetizioni) e gestione elementi del form

--- progetto_capolavoro/Form1.cs
+++ progetto_capolavoro/Form1.cs
@@ -10,105 +10,137 @@ using System.Windows.Forms;
 
 namespace progetto_capolavoro
 {
-    public partial class Form1 : Form
-    {
-        public string [] squadre;
-        int indexsqcasa;
-        int indexsqtrasf;
-        int dim;
-        public Form1()
-        {
-            InitializeComponent();
-            indexsqcasa = 0;
-            indexsqtrasf = 0;
-            dim = 19;
-            squadre = new string [20];
-            squadre[0] = "Atalanta";
-            squadre[1] = "Bologna";
-            squadre[2] = "Cagliari";
-            squadre[3] = "Empoli";
-            squadre[4] = "Fiorentina";
-            squadre[5] = "Frosinone";
-            squadre[6] = "Genoa";
-            squadre[7] = "Hellas Verona";
-            squadre[8] = "Inter";
-            squadre[9] = "Juventus";
-            squadre[10] = "Lazio";
-            squadre[11] = "Lecce";
-            squadre[12] = "Milan";
-            squadre[13] = "Monza";
-            squadre[14] = "Napoli";
-            squadre[15] = "Roma";
-            squadre[16] = "Salernitana";
-            squadre[17] = "Sassuolo";
-            squadre[18] = "Torino";
-            squadre[19] = "Udinese";
-           
-        }
+	public partial class Form1 : Form
+	{
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+		public List<string> Lsquadre;
+		public bool [] indici;
+		public int indexsqcasa;
+		public int indexsqtrasf;
+		public int npartite;
+		public Form1()
+		{
+			InitializeComponent();
+			indexsqcasa = 0;
+			indexsqtrasf = 0;
+			indici = new bool[20];
+			Lsquadre = new List<string>();
+			npartite= 0;
+			Lsquadre.Add("Atalanta");
+			Lsquadre.Add("Bologna");
+			Lsquadre.Add("Cagliari");
+			Lsquadre.Add("Empoli");
+			Lsquadre.Add("Fiorentina");
+			Lsquadre.Add("Frosinone");
+			Lsquadre.Add("Genoa");
+			Lsquadre.Add("Hellas Verona");
+			Lsquadre.Add("Inter");
+			Lsquadre.Add("Juventus");
+			Lsquadre.Add("Lazio");
+			Lsquadre.Add("Lecce");
+			Lsquadre.Add("Milan");
+			Lsquadre.Add("Monza");
+			Lsquadre.Add("Napoli");
+			Lsquadre.Add("Roma");
+			Lsquadre.Add("Salernitana");
+			Lsquadre.Add("Sassuolo");
+			Lsquadre.Add("Torino");
+			Lsquadre.Add("Udinese");
+			button2.Hide();
+		}
 
-        }
+		private void Form1_Load(object sender, EventArgs e)
+		{
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            listView1.Items.Clear();
-          
-            Random r = new Random();
-            indexsqcasa = r.Next(1, 20);
-            indexsqtrasf = r.Next(1, 20);
-            textBox1.Text = squadre[indexsqcasa];
-            RimuoviCasa();
-            textBox2.Text = squadre[indexsqcasa];
-            RimuoviTrasferta();
-            int golcasa = r.Next(1,5);
-            int goltrasf = r.Next(1, 5);
-            Partita partita = new Partita(textBox1.Text, textBox2.Text);
-            partita.AggiornaPunteggio(golcasa, goltrasf);
-            Campionato campionato = new Campionato();
-            campionato.AggiungiPartita(partita);
-            listView1.Items.Add($"{golcasa} - {goltrasf}");
-            listView2.Items.Add(partita.ToString());
-            MessageBox.Show("La partita è stata aggiunta al campionato");
-        }
+		}
 
-        public void RimuoviCasa()
-        {
-            for(int i = 0; i < 20; i++)
-            {
-                if (squadre[i] == squadre[indexsqcasa])
-                {
-                    squadre[indexsqcasa] = squadre[i + 1];
-                    squadre[i + 1] = squadre[indexsqcasa];
-                    dim--;
-                   
-                   
-                }
-               
-            }
-            MessageBox.Show("Sqadra di casa rimossa");
-        }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			listView1.Items.Clear();
 
-        public void RimuoviTrasferta()
-        {
-            for(int i = 0; i < 20; i++)
-            {
-                if (squadre[i] == squadre[indexsqtrasf])
-                {
-                    squadre[indexsqtrasf] = squadre[i + 1];
-                    squadre[i + 1] = squadre[indexsqtrasf];
-                    dim--;
-                   
-                   
-                }
-            }
-            MessageBox.Show("Sqadra in trasferta rimossa");
-        }
-        
-        private void button2_Click(object sender, EventArgs e)
-        {
-           
-        }
-    }
+			Random r = new Random();
+			npartite++;
+			if(npartite == 10)
+			{
+				MessageBox.Show("Hai aggiunto tutte le partite per questa giornata");
+				button1.Hide();
+				button2.Show();
+			}
+			indexsqcasa = EstIndici(r, indici);
+			indexsqtrasf = EstIndici(r, indici);
+			textBox1.Text = Lsquadre[indexsqcasa - 1];
+			textBox2.Text = Lsquadre[indexsqtrasf - 1];
+			int golcasa = r.Next(1, 5);
+			int goltrasf = r.Next(1, 5);
+			Partita partita = new Partita(textBox1.Text, textBox2.Text);
+			partita.AggiornaPunteggio(golcasa, goltrasf);
+			Campionato campionato = new Campionato();
+			campionato.AggiungiPartita(partita);
+			listView1.Items.Add($"{golcasa} - {goltrasf}");
+			listView2.Items.Add(partita.ToString());
+			
+				
+				
+			
+		}
+
+
+
+
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			
+			int ngiornata = 2;
+			listView1.Items.Clear();
+			listView2.Items.Clear();
+			textBox1.Text = "";
+			textBox2.Text = "";
+			button1.Show();
+			indexsqcasa = 0;
+			indexsqtrasf = 0;
+			indici = new bool[20];
+			Lsquadre = new List<string>();
+			npartite = 0;
+			Lsquadre.Add("Atalanta");
+			Lsquadre.Add("Bologna");
+			Lsquadre.Add("Cagliari");
+			Lsquadre.Add("Empoli");
+			Lsquadre.Add("Fiorentina");
+			Lsquadre.Add("Frosinone");
+			Lsquadre.Add("Genoa");
+			Lsquadre.Add("Hellas Verona");
+			Lsquadre.Add("Inter");
+			Lsquadre.Add("Juventus");
+			Lsquadre.Add("Lazio");
+			Lsquadre.Add("Lecce");
+			Lsquadre.Add("Milan");
+			Lsquadre.Add("Monza");
+			Lsquadre.Add("Napoli");
+			Lsquadre.Add("Roma");
+			Lsquadre.Add("Salernitana");
+			Lsquadre.Add("Sassuolo");
+			Lsquadre.Add("Torino");
+			Lsquadre.Add("Udinese");
+			label4.Text = $"GIORNATA {ngiornata}";
+			ngiornata++;
+		}
+
+
+		public int EstIndici(Random ran, bool[] ind)
+		{
+			int indice;
+			do
+			{
+				indice = ran.Next(1, 21);
+			} while (ind[indice - 1] == true);
+			ind[indice- 1] = true;
+	
+
+			return indice;
+			
+		}
+
+		
+	}
 }
